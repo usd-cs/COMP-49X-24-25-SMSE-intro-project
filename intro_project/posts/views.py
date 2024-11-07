1b654ba61d69feb2b2550748e52665fcea23b3f3
James (J.P.) Tasto <jtasto@sandiego.edu>
2024-11-07 15:37:01 -0800

    Test: testing branch for troubleshooting

--- intro_project/posts/views.py
+++ intro_project/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# This is solely to test to see if my (JP's) branches work
 
 def index(request):
     return HttpResponse("Hello, world. You're at the posts index.")
