93f3d5d75533881f120e4457220d901acbd4de41
scnbello <sbello@sandiego.edu>
2024-11-07 23:26:49 -0800

    Create Posts and Comment tests for database model

--- intro_project/posts/models.py
+++ intro_project/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Post(models.Model): 
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- intro_project/posts/tests.py
+++ intro_project/posts/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Comment
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        # Tests adding a user for posting
+        self.user = User.objects.create_user(username="testuser", password="password")
+
+    # Tests adding and saving post
+    @patch('posts.models.Post.save')
+    def test_add_post_mock_save(self, mock_save):
+        
+        post = Post.objects.create(
+            title="Mock Save Test",
+            content="Mock post content",
+            author=self.user
+        )
+
+        #post.save()
+        
+        # Tests that the save method works
+        mock_save.assert_called_once()
+
+
+class CommentModelTest(TestCase):
+    def setUp(self):
+        # Tests adding a user for commenting
+        self.user = User.objects.create_user(username="testuser", password="password")
+        self.post = Post.objects.create(
+            title="Mock comment Test",
+            content="Mock post content",
+            author=self.user
+        )
+    # Tests adding and saving comments
+    @patch('posts.models.Comment.save')
+    def test_add_comment_mock_save(self, mock_save):
+        comment = Comment.objects.create(
+            post=self.post,
+            content="Mock comment content.",
+            author=self.user
+        )
+
+        #comment.save()
+        # Tests that the save method works
+        mock_save.assert_called_once()
+
