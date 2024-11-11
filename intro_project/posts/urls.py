d1086262b4ab58ed067b8a849d353fc82d318b61
scnbello <sbello@sandiego.edu>
2024-11-10 17:58:55 -0800

    Handle login redirection and test updates for post creation

--- intro_project/posts/urls.py
+++ intro_project/posts/urls.py
@@ -5,5 +5,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< Updated upstream
     path("", views.index, name="index"),
+=======
+    path('admin/', admin.site.urls),
+    path("", views.index, name="index"), #home page
+    path('create_post/', views.create_post, name='create_post'),
+    #path('create_posts/', views.create_post, name='create_posts'),
+
+>>>>>>> Stashed changes
 ]
--- intro_project/posts/views.py
+++ intro_project/posts/views.py
@@ -5,8 +5,46 @@ Views File.
 from django.http import HttpResponse
 
 
+
 def index(request):
+<<<<<<< Updated upstream
+=======
+    #show all posts from the database
+    posts = Post.objects.all()
+    output = "\n".join([f"{post.title}: {post.content} : {post.author} : {post.created_at}" for post in posts])
+    return HttpResponse(output, content_type="text/plain")
+
+@csrf_exempt #bypasses Django's CSRF protection (TODO: look into this more)
+@login_required
+
+def create_post(request):
+>>>>>>> Stashed changes
     """
     Handles getting an HHTP request and sends a HTTP response
     """
+<<<<<<< Updated upstream
     return HttpResponse("Hello, world. You're at the posts index.")
+=======
+    if not request.user.is_authenticated:
+        return JsonResponse({"error": "Login is required."}, status=302) #redirects to login page by default
+
+    if request.method == "POST":
+        # parse JSON data
+        title = request.POST.get("title", "").strip()
+        content = request.POST.get("content", "").strip()
+
+        #check that data is valid
+        if not title or not content:
+            return JsonResponse({"error": "data needs to title  and content"}, status=400) #400 Bad Request (Client error response)
+
+        #creates the post
+        post = Post.objects.create(
+            title=title,
+            content=content,
+            author=request.user
+        )
+        return JsonResponse({"message": "You're logged in! Post created successfully.", "post_id": post.id}, status=201) #Created (Successful)
+
+    return JsonResponse({"error": "Invalid request method."}, status=405) # 405 Method not Allowed
+
+>>>>>>> Stashed changes
