66b5c3231f75f92b74ce8d31b85188caf4ab44e0
scnbello <sbello@sandiego.edu>
2024-11-10 18:26:00 -0800

    Merge views changes for routing posts

--- intro_project/posts/views.py
+++ intro_project/posts/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 
 
 
+
 def index(request):
 <<<<<<< Updated upstream
 =======
@@ -16,6 +17,7 @@ def index(request):
 
 @csrf_exempt #bypasses Django's CSRF protection (TODO: look into this more)
 @login_required
+<<<<<<< Updated upstream
 
 def create_post(request):
 >>>>>>> Stashed changes
@@ -25,6 +27,37 @@ def create_post(request):
 <<<<<<< Updated upstream
     return HttpResponse("Hello, world. You're at the posts index.")
 =======
+    if not request.user.is_authenticated:
+        return JsonResponse({"error": "Login is required."}, status=302) #redirects to login page by default
+
+    if request.method == "POST":
+        # parse JSON data
+        title = request.POST.get("title", "").strip()
+        content = request.POST.get("content", "").strip()
+
+        #check that data is valid
+        if not title or not content:
+            return JsonResponse({"error": "data needs to title  and content"}, status=400) #400 Bad Request (Client error response)
+
+        #creates the post
+        post = Post.objects.create(
+            title=title,
+            content=content,
+            author=request.user
+        )
+        return JsonResponse({"message": "You're logged in! Post created successfully.", "post_id": post.id}, status=201) #Created (Successful)
+
+    return JsonResponse({"error": "Invalid request method."}, status=405) # 405 Method not Allowed
+
+>>>>>>> Stashed changes
+=======
+
+def create_post(request):
+    """
+    Handles creating new posts and allows only logged in users to create a new post.
+    Args: request (HttpRequest): The Http request
+    Returns: JsonResponse: The JSON response shows either success or failure
+    """
     if not request.user.is_authenticated:
         return JsonResponse({"error": "Login is required."}, status=302) #redirects to login page by default
 
