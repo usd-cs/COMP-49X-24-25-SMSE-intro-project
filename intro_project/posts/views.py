431f0a391393fb90b65d43ea3c9a02b4ff1d7c9a
scnbello <sbello@sandiego.edu>
2024-11-12 13:48:02 -0800

    Add basic add post html for testing purpose

--- intro_project/posts/views.py
+++ intro_project/posts/views.py
@@ -1,15 +1,17 @@
 """
-Views File.
+Views File for handling post features in the post app.
 """
-#from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-
-
+from django.urls import reverse
+from django.views import generic
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from posts.models import Post
 
 
 def index(request):
-<<<<<<< Updated upstream
-=======
     #show all posts from the database
     posts = Post.objects.all()
     output = "\n".join([f"{post.title}: {post.content} : {post.author} : {post.created_at}" for post in posts])
@@ -17,40 +19,6 @@ def index(request):
 
 @csrf_exempt #bypasses Django's CSRF protection (TODO: look into this more)
 @login_required
-<<<<<<< Updated upstream
-
-def create_post(request):
->>>>>>> Stashed changes
-    """
-    Handles getting an HHTP request and sends a HTTP response
-    """
-<<<<<<< Updated upstream
-    return HttpResponse("Hello, world. You're at the posts index.")
-=======
-    if not request.user.is_authenticated:
-        return JsonResponse({"error": "Login is required."}, status=302) #redirects to login page by default
-
-    if request.method == "POST":
-        # parse JSON data
-        title = request.POST.get("title", "").strip()
-        content = request.POST.get("content", "").strip()
-
-        #check that data is valid
-        if not title or not content:
-            return JsonResponse({"error": "data needs to title  and content"}, status=400) #400 Bad Request (Client error response)
-
-        #creates the post
-        post = Post.objects.create(
-            title=title,
-            content=content,
-            author=request.user
-        )
-        return JsonResponse({"message": "You're logged in! Post created successfully.", "post_id": post.id}, status=201) #Created (Successful)
-
-    return JsonResponse({"error": "Invalid request method."}, status=405) # 405 Method not Allowed
-
->>>>>>> Stashed changes
-=======
 
 def create_post(request):
     """
@@ -78,6 +46,8 @@ def create_post(request):
         )
         return JsonResponse({"message": "You're logged in! Post created successfully.", "post_id": post.id}, status=201) #Created (Successful)
 
-    return JsonResponse({"error": "Invalid request method."}, status=405) # 405 Method not Allowed
+    elif request.method == "GET":
+        # Render a form for creating a post
+        return render(request, "posts/create_post.html")
 
->>>>>>> Stashed changes
+    return JsonResponse({"error": "Invalid request method."}, status=405) # 405 Method not Allowed
