5893056a8f928299bc9fcc42aab17f08d3d5c786
Shayna Bello <sbello@sandiego.edu>
2024-11-13 10:32:31 -0800

    Update urls.py removing login routes

--- intro_project/posts/urls.py
+++ intro_project/posts/urls.py
@@ -1,5 +1,5 @@
 """
-Url fil
+Url file
 """
 from django.contrib import admin
 from django.urls import path
@@ -9,10 +9,7 @@ from django.contrib.auth import views as auth_views
 app_name = "posts"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("", views.home, name="home"), #home page
     path('create_post/', views.create_post, name='create_post'),
-    path("login/", auth_views.LoginView.as_view(template_name="posts/login.html"), name="login"),  # Login page
-    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),  # Logout and redirect to home
     #path('create_posts/', views.create_post, name='create_posts'),
 ]
