b9f8d1c066f3706f70adb3c9058508de8ce8f47a
scnbello <sbello@sandiego.edu>
2024-11-12 19:23:47 -0800

    Change Index name to Home in all files

--- intro_project/posts/urls.py
+++ intro_project/posts/urls.py
@@ -4,10 +4,15 @@ Url fil
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = "posts"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index"), #home page
-    path('create_post/', views.create_post, name='create_post')
+    path("", views.home, name="home"), #home page
+    path('create_post/', views.create_post, name='create_post'),
+    path("login/", auth_views.LoginView.as_view(template_name="posts/login.html"), name="login"),  # Login page
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),  # Logout and redirect to home
     #path('create_posts/', views.create_post, name='create_posts'),
 ]
--- intro_project/posts/views.py
+++ intro_project/posts/views.py
@@ -1,20 +1,24 @@
 """
 Views File for handling post features in the post app.
 """
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 from django.views import generic
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from posts.models import Post
+from posts.models import Post, Comment
+# from .forms import PostForm, CommentForm
 
 
-def index(request):
-    #show all posts from the database
+def home(request):
+    """
+    Displays the home page with all the posts and comments. If a useris logged in, they can add posts and comments.
+    """
     posts = Post.objects.all()
     output = "\n".join([f"{post.title}: {post.content} : {post.author} : {post.created_at}" for post in posts])
+    #return render (request, "posts/home.html", {"posts": posts})
     return HttpResponse(output, content_type="text/plain")
 
 @csrf_exempt #bypasses Django's CSRF protection (TODO: look into this more)
