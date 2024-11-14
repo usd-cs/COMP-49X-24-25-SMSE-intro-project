821d9cc693a3477892624b83d21ff52ba75bd0dd
Cooor12 <nickzhou@sandiego.edu>
2024-11-14 01:07:29 -0800

    urls.py improved

--- intro_project/posts/urls.py
+++ intro_project/posts/urls.py
@@ -1,19 +1,28 @@
 """
 Url file configuration
 
-Defines the URLs for the app and connects each url to its corresponding view.
+Defines the URLs for the app and connects each URL to its corresponding view.
 """
-from django.contrib import admin
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
 app_name = "posts"
 
+# Define the URL patterns for the 'posts' app
 urlpatterns = [
+    # Home page to display all posts
     path('', views.home, name='home'),
+
+    # Create a new post
     path('create/', views.create_post, name='create_post'),
+
+    # Add a comment to a specific post
     path('<int:post_id>/comment/', views.create_comment, name='create_comment'),
+
+    # Delete a specific post (accessible only to superusers)
     path('<int:post_id>/delete/', views.delete_post, name='delete_post'),
+
+    # Delete a specific comment (accessible only to superusers)
     path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
